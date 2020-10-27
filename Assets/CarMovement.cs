5fa2fa20574f7ee02d40a8faa7c1892f34c223cb
jake willison <jakewillison23@gmail.com>
2020-10-27 01:16:19 -0400

    Completed Map
    This is the completed map with a fill in car.

---
+++ Assets/CarMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    Rigidbody m_Rigidbody;
+    float m_Speed;
+
+    void Start()
+    {
+        //Fetch the Rigidbody component you attach from your GameObject
+        m_Rigidbody = GetComponent<Rigidbody>();
+        //Set the speed of the GameObject
+        m_Speed = 15.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            m_Rigidbody.velocity = -transform.up * m_Speed;
+        if (Input.GetKey(KeyCode.S))
+            m_Rigidbody.velocity = transform.up * m_Speed;
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(0f, 0f, 1.5f);
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(0f, 0f, -1.5f);
+    }
+}
