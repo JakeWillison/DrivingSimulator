637309f8dbd843388c34ff3e526a9c6ddd7286d9
Dsnow21 <dfsnow01@indianatech.net>
2020-11-19 00:18:26 -0500

    Scoring system
    coin and scoring system installed

---
+++ Assets/Scoring.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoring : MonoBehaviour
+{
+    public GameObject scoreText;
+    public static int score;
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        score += 1;
+        scoreText.GetComponent<Text>().text = "Score: " + score;
+        Destroy(gameObject);
+    }
+
+}
