48958a67a896cdaf37ea654378f381087497bf94
jake willison <jakewillison23@gmail.com>
2020-11-19 19:57:38 -0500

    Added coins and the game over function
    I added 30 coins hidden throughout the level. When all 30 coins are collected the screen displays GAME OVER! and there is a score counter in the corner of the screen

--- Assets/Scoring.cs
+++ Assets/Scoring.cs
@@ -5,15 +5,20 @@ using UnityEngine.UI;
 
 public class Scoring : MonoBehaviour
 {
+    public GameObject endGameText;
     public GameObject scoreText;
     public static int score;
 
     void OnTriggerEnter(Collider other)
     {
-        
         score += 1;
         scoreText.GetComponent<Text>().text = "Score: " + score;
         Destroy(gameObject);
+
+        if(score == 30)
+        {
+            endGameText.SetActive(true);
+        }
     }
 
 }
